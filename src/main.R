82874b5e44997343d704177a46884ba6c7b6c3df
rcuevass <rogercuevas.ca@gmail.com>
2019-03-10 17:33:51 -0400

    adding code to compute chages in price

--- src/main.R
+++ src/main.R
@@ -4,8 +4,10 @@ source("utilities/functions.R")
 list.symbols <- get_symbols(path = "input/",file = "list.csv")
 
 
-df <- get_data_from_symbols(initial.date = "2019-02-01",
+df <- get_data_from_symbols(initial.date = "2019-01-01",
                             final.date = "2019-02-27",
                             list.symbols = list.symbols)
-df$df.tickers
+
+View(df)
+
 
--- utilities/functions.R
+++ utilities/functions.R
@@ -23,13 +23,30 @@ get_data_from_symbols <- function(initial.date,
   # set tickers
   tickers <- list.symbols
   
-  l.out <- BatchGetSymbols(tickers = tickers, 
+  df <- BatchGetSymbols(tickers = tickers, 
                            first.date = initial.date,
                            last.date = final.date, 
                            freq.data = frequency.data,
                            cache.folder = file.path(tempdir(), 
                                                     'BGS_Cache') ) # cache in tempdir()
   
+  # Select only relevant dataframe
+  df <- df$df.tickers
   
-  return(l.out)
+  # Select needed columns
+  df <- df %>%
+    dplyr::select(ticker,ref.date,price.close,volume)
+  
+  
+  df <- df %>%
+    dplyr::group_by(ticker) %>%
+    dplyr::mutate(diff.price.close = price.close - lag(price.close)) %>%
+    dplyr::mutate(rel.diff.price.close = 100*diff.price.close/price.close)
+  
+  return(df)
 }
+
+
+# http://stat545.com/block023_dplyr-do.html
+
+# https://stackoverflow.com/questions/14846547/calculate-difference-between-values-in-consecutive-rows-by-group
