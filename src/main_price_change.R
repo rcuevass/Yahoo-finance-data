82196de90dfb4c14658a2332f93b05404b9f5c2d
rcuevass <rogercuevas.ca@gmail.com>
2019-03-10 19:41:07 -0400

    adding column for currency for each ticker

--- src/main_price_change.R
+++ src/main_price_change.R
@@ -2,6 +2,7 @@ source("utilities/functions.R")
 
 list.symbols <- get_symbols(path = "input/",file = "list.csv")
 
+
 df <- get_data_from_symbols(initial.date = "2018-01-01",
                             final.date = "2019-02-27",
                             list.symbols = list.symbols,
